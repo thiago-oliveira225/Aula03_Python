a65b0c1b633a40695befe65165010b15dc550cd4
Thiago Oliveira <thiagoliveirasax@gmail.com>
2023-10-03 16:27:36 -0300

    primeiro commit

---
+++ python.py
@@ -0,0 +1,15 @@
+print ("########## Identificador de Sinal ###########")
+
+numero = int(input("Digite um número = "))
+
+if (numero > 0):
+    print ("Número positivo")
+elif (numero < 0):
+    print ("Número negativo")
+else:
+    print ("O Número é zero")
+
+
+
+
+
