45dbe82fa796af2f597ff7520bdb61c70a3442d5
Thiago Oliveira <146132069+thiago-oliveira225@users.noreply.github.com>
2023-10-23 14:21:53 -0300

    Delete Aula09 directory

--- Aula09/exercicio04.py
+++
@@ -1,7 +0,0 @@
-class Pedido:   
-    def __init__(self,cliente,itens, valor_total): ## método construtor
-        self.cliente = cliente
-        self.itens = itens
-        self.valor_total = valor_total
-
-
--- Aula09/menu.py
+++
@@ -1,8 +0,0 @@
-    
-def menu():
-    print("Escolha uma opção \n")
-    print("Opção 1: Adição \n")
-    print("Opção 2: Subtração \n")
-    print("Opção 3: Multiplicação \n")
-    print("Opção 4: Divisão \n")
-    print("Opção 5: Sair \n")
