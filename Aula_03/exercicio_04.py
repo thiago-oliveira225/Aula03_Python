405cdcb9cde5afac98931005f24e27e265a6b8cb
Thiago Oliveira <146132069+thiago-oliveira225@users.noreply.github.com>
2023-10-23 14:22:10 -0300

    Delete Aula_03 directory

--- Aula_03/exercicio_04.py
+++
@@ -1,9 +0,0 @@
-dia = input("Digite um dia da semana no formato/ Exemplo: Segunda-Feira =")
-
-
-if (dia == "Segunda-Feira" or dia == "Terça-Feira" or dia == "Quarta-Feira" or dia == "Quinta-Feira" or dia == "Sexta-Feira"):
-    print("Dia útil")
-elif (dia == "Sábado" or dia == "Domingo"):
-    print("Final de Semana")
-else:
-    print("Formato inválido")
