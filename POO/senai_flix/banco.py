6901a613303e791caae71184553d50e5bef1a0f8
Thiago Oliveira <146132069+thiago-oliveira225@users.noreply.github.com>
2023-10-25 17:12:39 -0300

    Delete POO/senai_flix directory

--- POO/senai_flix/banco.py
+++
@@ -1,9 +0,0 @@
-from generos import Filmes_Generos
-
-filmes_acao = {"Item":"Filme A","Valor":12.50,"Item":"Filme B","Valor":10.50,"Item":"Filme C","Valor":15.00,}
-filmes_suspense = {"Item":"Filme D","Valor":12.50,"Item":"Filme E","Valor":10.50,"Item":"Filme F","Valor":15.00,}
-
-class Banco(Filmes_Generos):
-
-    def pagamento(self):
-        
