85f65cab534c3c93e3c3e646f1a007640876ae52
Thiago Oliveira <146132069+thiago-oliveira225@users.noreply.github.com>
2023-11-01 16:05:52 -0300

    Update exercicio05.py

--- Aula11/exercicio05.py
+++ Aula11/exercicio05.py
@@ -1,37 +1,86 @@
-funcionarios = {}
+funcionarios = {} ## DICIONARIO VAZIO PARA INCLUIR OS FUNCIONARIOS
 
 
-class Funcionario:
+class Funcionario: ## CLASSE MÃE
 
-    def __init__(self, nome, salario_base):
+    def __init__(self, nome, salario_base): ## METODO CONSTRUTOR COMUM PARA TODOS OS TIPOS DE FUNCIONARIOS
         self.nome = nome
         self.salario_base = salario_base
 
-    def calcular_salario(self, nome, salario_base):
-        if nome not in funcionarios:
-            funcionarios[nome] = {"Salário": salario_base}
-        else:
-            print("Funcionário já existe no sistema")
-
-
-class CLT(Funcionario):
-   def calcular_salario(self):
+    def calcular_salario(self): ## APENAS ATRELANDO ESSE METODO NA CLASSE MAE PARA SER UTILIZADO PARA AS CLASSES FILHAS
         pass
+
+
+class CLT(Funcionario): ## CLASSE FILHA
+   def calcular_salario(self):
+        funcionarios[self.nome] = {"Contrato":"CLT","Salário": (self.salario_base)}
+        
+
+class Estagiario(Funcionario):
+
+    def calcular_salario(self):
+        funcionarios[self.nome] = {"Contrato":"Estágio","Salário": self.salario_base / 2}
+        
+
 
 class Comissionado(Funcionario):
+    def __init__(self, nome, salario_base, comissao):
+        self.comissao = comissao
+        super().__init__(nome, salario_base)
+
     def calcular_salario(self):
-        pass
+        funcionarios[self.nome] = {"Contrato":"Comissionado","Salário": self.salario_base + self.comissao}
+        
+
+## PROGRAMA MAIN
+
+while True:
+    print(" Escolha uma opção = ")
+    print(" Opção 1. Incluir Funcionário CLT ")
+    print(" Opção 2. Incluir Funcionario Comissionado ")
+    print(" Opção 3. Incluir Funcionário Estagiário ")
+    print(" Opção 4. Mostrar todos os Funcionários ")
+    print(" Opção 5. Sair \n")
     
-class Estagiario(Funcionario):
-    def calcular_salario(self):
-        pass    
-      
 
-clt = CLT("Thiago", "3500")
-comissionado = Comissionado()
-estagiario = Estagiario()
+    opcao = input("")
+
+    match opcao:
+    
+        case "1":       
+
+            clt = CLT(input ("Digite o nome do Funcionario = "), int(input ("Digite o salário do Funcionario = ")))
+
+            clt.calcular_salario()
+
+            print (f"\n#### Funcionário incluído com sucesso ####\n")
+
+        case "2": 
+
+            comissionado = Comissionado((input ("Digite o nome do Funcionario = ")), int(input ("Digite o salário do Funcionario = ")), int(input ("Digite a comissao do Funcionario = ")))
+
+            comissionado.calcular_salario()
+
+            print (f"\n#### Funcionário incluído com sucesso ####\n")
+
+        case "3": 
+
+            estagiario = Estagiario (input ("Digite o nome do Funcionario = "), int(input ("Digite o salário referencia do CLT = ")))
+
+            estagiario.calcular_salario()
+
+            print (f"\n#### Funcionário incluído com sucesso ####\n")
+
+        case "4": 
+
+            print (funcionarios)
 
 
-print(clt.calcular_salario())
-print(comissionado.calcular_salario())
-print(estagiario.calcular_salario())
+        case "5": 
+
+            print("#######  Obrigado por utilizar o Programa de Funcionários ####### ")
+
+            break
+
+        case _:
+            print("\nEscolha uma opção válida !!!!\n")
