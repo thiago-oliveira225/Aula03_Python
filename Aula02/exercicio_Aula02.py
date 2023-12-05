1cbba1bf9b86fcac6953b1cac83a0cde8281eb81
Thiago Oliveira <146132069+thiago-oliveira225@users.noreply.github.com>
2023-12-04 21:21:30 -0300

    Add files via upload

---
+++ Aula02/exercicio_Aula02.py
@@ -0,0 +1,32 @@
+numero_1 = 5
+numero_2 = 6
+resultado = numero_1 + numero_2
+print(resultado)
+
+#Calculadora de Python com inserção manual de valores
+
+print("\n--------Bem vindo a calculadora Python--------")
+print("--------By IgorLeon--------")
+
+numero_3 = int(input("\nDigite o valor do primeiro numero: ")) #Primeiro valor para o calculo
+numero_4 = int(input("Digite o segundo numero: ")) #Segundo valor para o calculo
+resultado_1 = numero_3 + numero_4 #Calculo de Soma
+print (f"\nResultado: {resultado_1}") #Exibir Resultado da Soma
+print ("--------fim--------")
+
+#Calculadora de Python com inserção manual de valores (+, -, *, /)
+
+print("\n--------Bem vindo a calculadora Python--------")
+print("--------By IgorLeon--------")
+
+numero_5 = int(input("\nDigite o valor do primeiro numero: "))
+numero_6 = int(input("Digite o valor do segundo numero: "))
+resultado_Soma = numero_5 + numero_6
+resultado_Sub = numero_5 - numero_6
+resultado_Mult = numero_5 * numero_6
+resultado_Div = numero_5 / numero_6
+print (f"\nResultado da Soma: {resultado_Soma}")
+print (f"Resultado da Sub: {resultado_Sub}")
+print (f"Resultado da Mult: {resultado_Mult}")
+print (f"Resultado da Div: {resultado_Div}")
+print ("--------fim--------")
